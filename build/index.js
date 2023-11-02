febe480f03bb79a82a970cc798a66d8d8b22b354
Ylnom <nguyenlinh2k31111@gmail.com>
2023-11-02 10:57:02 +0700

    init project

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let a = 10;
+let b = 11;
+let c = 12;
+let d = 13;
+console.log(a);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let a :number = 10
+let b :number = 11
+let  c: number =12
+let d : number = 13
+console.log(a);
